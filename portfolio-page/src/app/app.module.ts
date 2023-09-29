9601f5c7ba85097ac37e2c9f84d13e8ae16ebf38
Keijo Alexander Nierula <keijo.nierula@students.bfh.ch>
2023-09-29 16:47:37 +0200

    add angular material and basic layout

--- portfolio-page/src/app/app.module.ts
+++ portfolio-page/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
